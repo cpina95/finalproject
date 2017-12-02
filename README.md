1b5afb7a43576d717f03fe0d7e77f941fef3b2c2
cpina95 <cpina95@ufl.edu>
2017-12-01 22:32:22 -0500

    week2-final

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-Week 1: 
+Week 1:
 This week, I created my HTML file for my project, and I added a little CSS because the plainness of the file without any was driving me crazy. As of right now, the site looks pretty basic, but once I start adding CSS grid and possibly some bootstrap styles also, it'll look much more impressive.
+
+Week 2:
+This week, I set out to add Javascript to my HTML. I added a few graphs and the toggle effect I wanted to include on my HTML. I originally wanted to add graphs about the human rights issues in Cuba and the statistics that go with them, but I found it a lot harder to find concrete, numerical statistics. In my prior research, there was information about the human rights issues, but not enough that I could put on a graph. The only Javascript I have left that I want to add is creating buttons that would make my site more interactive. I'm thinking about making buttons for the four subsections under culture, and also for the five subsections under Human Rights. I'm just kind of stuck on how to do that.
+
+This upcoming week, I'm going to finish up the CSS for the HTML. Ryan gave me a few tips to make my site more visually appealing, and I'm going to attempt to incorporate CSS Grid or Bootstrap. I've visualized the site looking better than it is so far, but I'm finding myself struggling to transfer what I'm picturing onto the site.
--- js/script.js
+++ js/script.js
@@ -3,52 +3,127 @@ $(document).ready(function() {
 
   // function to hide all divs
   function hideAll() {
-    $('#allthatweneeded').hide();
-    $('#everysecondcounts').hide();
-    $('#shouldvegonetobed').hide();
-    $('#americannights').hide();
+    $('#dancingText').hide();
+    $('#foodText').hide();
+    $('#baseballText').hide();
   }
 
   // run that function right away
   hideAll();
 
-
-  $('#theText').slideToggle();
-
-  // when any animal is clicked, make the suitable div appear
-  $('.albumIcon').click(function() {
-
-    $("audio").each(function() {
-        this.pause();
-        this.currentTime = 0;
-    });
+  $('#dancingText').slideToggle();
+  $('#foodText').slideToggle();
+  $('#baseballText').slideToggle();
 
     // but first, hide all the divs to ensure that
     // only one will be open, ever
     hideAll();
 
-    // here is a switch statement - this was in Codecademy
-    // "Control Flow" lesson 7
-
-    // we can get the ID of the thing that was clicked -
-    switch ($(this).attr("id")) {
-      case "allthatweneededpic":
-        $('#allthatweneeded').show();
-        break;
-      case "everysecondcountspic":
-        $('#everysecondcounts').show();
-        break;
-      case "shoudlvegonetobedpic":
-        $('#shouldvegonetobed').show();
-        break;
-      case "americannightspic":
-        $('#americannights').show();
-        break;
-    }
-  }); // end of function for clicking
-
-  $('#bandpic').click(function() {
-    $('#theText').slideToggle();
+  $('#dancingpic').click(function() {
+    $('#dancingText').slideToggle();
   });
 
+  $('#foodpic').click(function() {
+    $('#foodText').slideToggle();
+  });
+
+  $('#baseballpic').click(function() {
+    $('#baseballText').slideToggle();
+  });
+
+});
+
+Highcharts.chart('piechart', {
+    chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie'
+    },
+    title: {
+        text: 'Ethnicity in Cuba'
+    },
+    tooltip: {
+        pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: true,
+                format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                style: {
+                    color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Ethnicity',
+        colorByPoint: true,
+        data: [{
+            name: 'White',
+            y: 64.1
+        }, {
+            name: 'Mulatto (mixed)',
+            y: 26.6,
+            sliced: true,
+            selected: true
+        }, {
+            name: 'Black',
+            y: 9.3
+        }]
+    }]
+});
+
+Highcharts.chart('agegraph', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Age Structure'
+    },
+    subtitle: {
+        text: 'Source: https://www.cia.gov/library/publications/the-world-factbook/geos/cu.html'
+    },
+    xAxis: {
+        categories: [
+            '0-14',
+            '15-24',
+            '25-54',
+            '55-64',
+            '65-over',
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Population'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f}</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'Male',
+        data: [95870, 706882, 2490483, 640150, 763058]
+
+    }, {
+        name: 'Female',
+        data: [896476, 655446, 2462250, 679603, 902189]
+
+    }]
 });
